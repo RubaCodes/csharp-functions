0f84b39dc4c1304577768e18a8e908c0cb9a77a2
RubaCodes <rubacodes@gmail.com>
2022-09-17 14:32:42 +0200

    esercizio base

--- Program.cs
+++ Program.cs
@@ -1,1 +1,51 @@
 ﻿//c-sharp functions
+
+
+//void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]“.
+void StampaArray(int[] array) {
+    Console.Write('[');
+
+    for (int i = 0; i < array.Length; i++) {
+
+        Console.Write(array[i] + " , ");
+    }
+    Console.Write(']');
+    Console.WriteLine();
+}
+//funzione genera quadrati
+int Quadrato(int numero) {
+    return (int)Math.Pow(numero, 2);
+}
+//funzione quadrati in array
+int[] ElevaArrayAlQuadrato(int[] array) {
+    int[] localArray = (int[])array.Clone();
+    for (int i = 0; i < localArray.Length; i++) {
+        localArray[i] = Quadrato(localArray[i]);
+    }
+    return localArray;
+ }
+//funzione somma elementi array
+int sommaElementiArray(int[] array) {
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++) {
+        sum += array[i];
+    }
+    return sum;
+
+}
+//array di test
+int[] test = { 2, 6, 7, 5, 3, 9 };
+Console.WriteLine("--Array di partenza--");
+StampaArray(test);
+Console.WriteLine("--Array elevato al quadrato--");
+int[] arrayElevato = ElevaArrayAlQuadrato(test);
+StampaArray(arrayElevato);
+Console.WriteLine("--Array di partenza dopo l'elevazione--");
+StampaArray(test);
+Console.WriteLine("--Array di partenza sommato--");
+int arrayTestSommato = sommaElementiArray(test);
+Console.WriteLine(arrayTestSommato);
+Console.WriteLine("--Array elevato al quadrato sommato--");
+int arrayElevatoSommato = sommaElementiArray(arrayElevato);
+Console.WriteLine(arrayElevatoSommato);
+
